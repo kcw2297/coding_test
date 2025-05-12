61a389d0908c9507391235d136e722378b955d52
kcw2297 <kcw2371@gmail.com>
2025-05-12 15:20:26 +0900

    feat: implement => 프로그래머스 lvl3 셔틀버스

---
+++ 프로그래머스/lvl3/imple_셔플버스.py
@@ -0,0 +1,32 @@
+# 링크 => https://school.programmers.co.kr/learn/courses/30/lessons/17678
+
+
+
+from collections import deque
+
+def solution(n, t, m, timetable):
+    crew_times = sorted(
+        int(h) * 60 + int(minute)
+        for time in timetable
+        for h, minute in [time.split(':')]
+    )
+
+    shuttle_times = [540 + i * t for i in range(n)]
+
+    crew = deque(crew_times)
+    
+    last_possible = 0
+    for depart in shuttle_times:
+        boarded = []
+        while crew and crew[0] <= depart and len(boarded) < m:
+            boarded.append(crew.popleft())
+   
+        if len(boarded) < m:
+            last_possible = depart
+        else:
+            last_possible = boarded[-1] - 1
+
+   
+    hh = last_possible // 60
+    mm = last_possible % 60
+    return f"{hh:02d}:{mm:02d}"
