41fdbbee44d090656c77ad7d80c33107b3accc94
kcw2297 <kcw2371@gmail.com>
2025-05-23 10:34:50 +0900

    feat: backtrack => 프로그래머스 lvl3 여행 경로

---
+++ 프로그래머스/lvl3/backtrack_여행경로.py
@@ -0,0 +1,52 @@
+# 링크 => https://school.programmers.co.kr/learn/courses/30/lessons/43164
+
+
+from collections import defaultdict
+
+def solution(tickets):
+    graph = defaultdict(list)
+
+    for a, b in tickets:
+        graph[a].append(b)
+
+    for airport in graph:
+        graph[airport].sort(reverse=True)
+
+    stack = ['ICN']   
+    path = []       
+
+    while stack:
+        top = stack[-1]
+        if graph[top]:
+            next_airport = graph[top].pop()
+            stack.append(next_airport)
+        else:
+            path.append(stack.pop())
+
+    return path[::-1]
+
+
+
+
+
+"""
+시간 복잡도: O(E logE)
+- E => ticket 수
+- V => 공항 수
+
+graph 생성 => O(E)
+공항별 정렬 => O(E logE)
+오일러 트레일 => O(E)
+O(E) + O(E logE) + O(E) => O(E logE) (상수 제거)
+
+래프의 오일러 트레일(Eulerian trail) 알고리즘
+- 그래프의 모든 간선(edge)를 정확히 한번씩 통과하는 경로를 구하는 알고리즘
+
+오일러 트레일 방식
+- edge를 따라 가능한 깊게 따라간다
+- 더 이상 나갈 간선이 없으면, 그 정점을 경로에 추가(backtrack)
+- 이 후, 남은 간선이 있을 때까지 반복
+
+First Trial [X]
+"""
+
