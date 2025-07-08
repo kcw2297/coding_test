e50f5bfd038629e286d250ba8131ea66cebc9a66
kcw2297 <kcw2371@gmail.com>
2025-07-08 09:34:33 +0900

    feat: dp => 프로그래머스 lvl3 거스름돈

---
+++ 프로그래머스/lvl3/dp_거스름돈.py
@@ -0,0 +1,14 @@
+# 링크 => https://school.programmers.co.kr/learn/courses/30/lessons/12907
+
+
+def solution(n, money):
+    MOD = 1_000_000_007
+    dp = [0] * (n + 1)
+    dp[0] = 1  
+    
+    for coin in money:
+        for i in range(coin, n + 1):
+            dp[i] = (dp[i] + dp[i - coin]) % MOD
+
+    return dp[n]
+
